f5e35ddaf8f8d14418f1cd76f0ca609e01d7a1ad
LimingRenPA <liming.ren@projectarcher.com>
2023-11-22 10:55:13 -0800

    Initial Commit

---
+++ ArcherQATestUITests/ArcherQATestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArcherQATestUITestsLaunchTests.swift
+//  ArcherQATestUITests
+//
+//  Created by Liming Ren on 11/22/23.
+//
+
+import XCTest
+
+final class ArcherQATestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
